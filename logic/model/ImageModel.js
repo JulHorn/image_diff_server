910610221948b344c4d0d966386d623c3a589d23
julian.horn <julian.horn@aoe.com>
2016-11-01 16:47:17 +0100

    Adds some comments.

--- logic/model/ImageModel.js
+++ logic/model/ImageModel.js
@@ -1,3 +1,6 @@
+/**
+ * Constructor.
+ * **/
 var ImageModel = function () {
     this.height = 0;
     this.width = 0;
@@ -7,42 +10,87 @@ var ImageModel = function () {
 
 /* ----- Getter ----- */
 
+/**
+ * Returns the image height.
+ *
+ * @return Returns the height.
+ * **/
 ImageModel.prototype.getHeight = function () {
     return this.height;
 };
 
+/**
+ * Returns the image width.
+ *
+ * @return Returns the image width.
+ * **/
 ImageModel.prototype.getWidth = function () {
     return this.width;
 };
 
+/**
+ * Returns the image name together with its suffix.
+ *
+ * @return Returns the image name together with its suffix.
+ * **/
 ImageModel.prototype.getName = function () {
     return this.name;
 };
 
+/**
+ * Returns the image path including the image name and its suffix.
+ *
+ * @return Returns the image path including the image name and its suffix.
+ * **/
 ImageModel.prototype.getPath = function () {
     return this.path;
 };
 
 /* ----- Setter ----- */
 
+/**
+ * Sets the image height.
+ *
+ * @param height Sets the image height.
+ * **/
 ImageModel.prototype.setHeight = function (height) {
     this.height = height;
 };
 
+/**
+ * Sets the image width.
+ *
+ * @param width Sets the image width.
+ * **/
 ImageModel.prototype.setWidth = function (width) {
     this.width = width;
 };
 
+/**
+ * Sets the image name including its type suffix.
+ *
+ * @param name Sets the image name.
+ * **/
 ImageModel.prototype.setName = function (name) {
     this.name = name;
 };
 
+/**
+ * Sets the image path.
+ *
+ * @param path Sets the image path.
+ * **/
 ImageModel.prototype.setPath = function (path) {
     this.path = path;
 };
 
 /* ----- Others ----- */
 
+/**
+ * Loads the given data with an image structure into this image.
+ *
+ * @param data The object containing the image data. Must have the structure of this image object.
+ * **/
 ImageModel.prototype.load = function (data) {
     this.setPath(data.path);
     this.setName(data.name);
--- logic/model/ImageSetModel.js
+++ logic/model/ImageSetModel.js
@@ -1,6 +1,9 @@
 var uuid = require('node-uuid');
 var ImageModel = require('./ImageModel');
 
+/**
+ * Constructor. Adds empty ImageModel objects as its reference, new and diff images.
+ * **/
 var ImageSetModel = function () {
     this.difference = 0;
     this.distance = 0;
@@ -13,62 +16,132 @@ var ImageSetModel = function () {
 
 /* ----- Getter ----- */
 
+/**
+ * Returns the percentual pixel difference of the reference and new image.
+ *
+ * @return Returns the percentual pixel difference of the reference and new image.
+ * **/
 ImageSetModel.prototype.getDifference = function () {
     return this.difference;
 };
 
+/**
+ * Returns the distance of the reference and new image.
+ *
+ * @return Returns the distance difference of the reference and new image.
+ * **/
 ImageSetModel.prototype.getDistance = function () {
     return this.distance;
 };
 
+/**
+ * Returns the error text.
+ *
+ * @return Returns the error text.
+ * **/
 ImageSetModel.prototype.getError = function () {
     return this.error;
 };
 
+/**
+ * Returns the id.
+ *
+ * @return Returns the id.
+ * **/
 ImageSetModel.prototype.getId = function () {
     return this.id;
 };
 
+/**
+ * Returns the reference image.
+ *
+ * @return Returns the reference image.
+ * **/
 ImageSetModel.prototype.getReferenceImage = function () {
     return this.referenceImage;
 };
 
+/**
+ * Returns the new image.
+ *
+ * @return Returns the new image.
+ * **/
 ImageSetModel.prototype.getNewImage = function () {
     return this.newImage;
 };
 
+/**
+ * Returns the diff image.
+ *
+ * @return Returns the diff image.
+ * **/
 ImageSetModel.prototype.getDiffImage = function () {
     return this.diffImage;
 };
 
 /* ----- Setter ----- */
 
+/**
+ * Sets the percentual pixel difference between the reference and new image.
+ *
+ * @param difference The difference between the reference and new image.
+ * **/
 ImageSetModel.prototype.setDifference = function (difference) {
     this.difference = difference;
 };
 
+/**
+ * Sets the distance between the reference and new image.
+ *
+ * @param distance The distance between the reference and new image.
+ * **/
 ImageSetModel.prototype.setDistance = function (distance) {
     this.distance = distance;
 };
 
+/**
+ * Sets the error reason.
+ *
+ * @param error The error reason.
+ * **/
 ImageSetModel.prototype.setError = function (error) {
     this.error = error;
 };
 
+/**
+ * Sets the reference image.
+ *
+ * @param image The reference image.
+ * **/
 ImageSetModel.prototype.setReferenceImage = function (image) {
     this.referenceImage = image;
 };
 
+/**
+ * Sets the new image.
+ *
+ * @param image The new image.
+ * **/
 ImageSetModel.prototype.setNewImage = function (image) {
     this.newImage = image;
 };
 
+/**
+ * Sets the diff image.
+ *
+ * @param image The diff image.
+ * **/
 ImageSetModel.prototype.setDiffImage = function (image) {
     this.diffImage = image;
 };
 
 /* ----- Other ----- */
 
+/**
+ * Loads the given data with an image set structure into this image set.
+ *
+ * @param data The object containing the image set data. Must have the structure of this image set object.
+ * **/
 ImageSetModel.prototype.load = function (data) {
     this.setDistance(data.distance);
     this.setDifference(data.difference);
