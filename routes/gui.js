48b32b742891b7a4cb8fc569285cc4eb18bb2f30
julian.horn <julian.horn@aoe.com>
2017-01-24 08:26:08 +0100

    Adds some more information to the ui.

--- routes/gui.js
+++ routes/gui.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var ImageMetaInformationModel = require('../logic/model/ImageMetaInformationModel');
 var Image = require('../logic/model/ImageModel');
 var path = require('path');
+var configuration = require('../logic/ConfigurationLoader');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Image Diff Server', data: ImageMetaInformationModel });
+    res.render('index', { title: 'Image Diff Server', data: ImageMetaInformationModel, config: configuration });
 });
 
 module.exports = router;
