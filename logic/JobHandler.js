b3cc7694689c990ba8f6e4b7dd51a43675f73a18
julian.horn <gimme@posteo.de>
2017-01-27 23:39:45 +0100

    Removes some direct prototype accesses which I missed the last time.

--- logic/JobHandler.js
+++ logic/JobHandler.js
@@ -34,16 +34,20 @@ JobHandler.prototype.getRunningJob = function () {
  * @param job The job which will be added/discarded to the queue, depending on the execution mode.
  * **/
 JobHandler.prototype.addJob = function (job) {
-    // Discard job if something is already in the queue
-    if(this.behaviour == 0
-        && (this.jobQueue.length > 1
-        || this.isJobRunning)) {
-        logger.info('There is already a job being executed. Execution behaviour is set to discard. Discarding new job.');
-    // Add job to queue
-    } else {
-        logger.info('Adding new job to the queue:', job.prototype.getJobName());
-        this.jobQueue.push(job);
-        this.__executeJob();
+    try {
+        // Discard job if something is already in the queue
+        if (this.behaviour == 0
+            && (this.jobQueue.length > 1
+            || this.isJobRunning)) {
+            logger.info('There is already a job being executed. Execution behaviour is set to discard. Discarding new job.');
+            // Add job to queue
+        } else {
+            logger.info('Adding new job to the queue:', job.getJobName());
+            this.jobQueue.push(job);
+            this.__executeJob();
+        }
+    } catch (exception){
+        logger.error(exception);
     }
 };
 
@@ -55,22 +59,26 @@ JobHandler.prototype.addJob = function (job) {
 JobHandler.prototype.__executeJob = function() {
     var that = this;
 
-    // If there exists a job, then change the state to isRunning and execute the job
-    if(this.jobQueue.length >= 1 && !this.isJobRunning) {
-        this.isJobRunning = true;
-        this.runningJob = this.jobQueue.shift();
+    try {
+        // If there exists a job, then change the state to isRunning and execute the job
+        if (this.jobQueue.length >= 1 && !this.isJobRunning) {
+            this.isJobRunning = true;
+            this.runningJob = this.jobQueue.shift();
 
-        // ToDo: Error handling: If something happens, the job locks the execution queue forever
-        logger.info('Executing job:', this.runningJob.prototype.getJobName());
-        this.runningJob.execute(function () {
-            that.isJobRunning = false;
-            that.runningJob = null;
+            // ToDo: Error handling: If something happens, the job locks the execution queue forever
+            logger.info('Executing job:', this.runningJob.getJobName());
+            this.runningJob.execute(function () {
+                that.isJobRunning = false;
+                that.runningJob = null;
 
-            // Execute the next job
-            that.__executeJob();
-        });
-    } else {
-        // ToDo: Some Error Handling or do nothing because there is nothing to do?
+                // Execute the next job
+                that.__executeJob();
+            });
+        } else {
+            // ToDo: Some Error Handling or do nothing because there is nothing to do?
+        }
+    } catch (exception) {
+        logger.error(exception);
     }
 };
 
