5e49a75e36d9a6592ca31c43fb77aa50041a38e8
julian.horn <gimme@posteo.de>
2017-01-27 23:41:36 +0100

    Removes one long unneeded comment.

--- logic/job/Job.js
+++ logic/job/Job.js
@@ -15,42 +15,6 @@ var Job = function(jobName, callback) {
     this.imageManipulator = new ImageManipulator();
 };
 
-/*
- um die js vererbung noch mal schön auf dem papier zu sehen - so kann das dann aussehen...
- var Job = function() {}
- Job.prototype.foo ...
- var Job = require('/Job')
- var SpecialJob = function() {
- Job.call()
- }
- SpecialJob.prototype = Object.create(Job.prototype)
-
-
-
-
-
-
- var WidgetBase = function($widgetContainer, widgetSetup) {
- this.widgetSetup = widgetSetup;
- this.$widgetContainer = $widgetContainer;
- ...
- this.init();
- this.bindEvents();
- }
- WidgetBase.prototype.init = function() {
- ...
- }
- WidgetBase.prototype.bindEvents = function() {
- ...
- }
- var WidgetBase = require('/base');
- var Widget = function($widgetContainer, widgetSetup) {
- WidgetBase.call(this, $widgetContainer, widgetSetup);
- };
- Widget.prototype = Object.create(WidgetBase.prototype);
-*/
-
-
 /* ----- Getter ----- */
 
 /**
