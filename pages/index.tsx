a88594a87cbb2286fde7303ee3eca1315045bb0c
julian.horn <julian.horn@aoe.com>
2019-06-04 16:22:04 +0200

    Transforms class components to function components.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,13 @@ import * as React from 'react'
 import Header from "./header/header";
 import Footer from "./footer/footer";
 
-class IndexPage extends React.Component {
-	render() {
-		return (
-			<div>
-				<Header jobName="TestJob" lastJobFinished="10.10.10" maxPixDiff={100} progress={100} />
-				<Footer />
-			</div>
-		)
-	}
-}
+const IndexPage: React.FunctionComponent<{}>  = ({}): React.ReactElement => {
+	return (
+		<div>
+			<Header jobName="TestJob" lastJobFinished="10.10.10" maxPixDiff={100} progress={100} />
+			<Footer />
+		</div>
+	)
+};
 
 export default IndexPage
