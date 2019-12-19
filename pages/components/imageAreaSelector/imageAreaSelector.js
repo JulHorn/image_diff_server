b437f7ea492104d8928f968418960e1b1e046962
julian.horn <julian.horn@aoe.com>
2019-12-19 17:37:28 +0100

    - Some minor style changes - Some more implementation for the image area selection

--- pages/components/imageAreaSelector/imageAreaSelector.js
+++ pages/components/imageAreaSelector/imageAreaSelector.js
@@ -2,12 +2,48 @@ import React, {useState} from 'react'
 import css from "./imageAreaSelector.scss";
 
 const ImageAreaSelector = ({ state }) => {
+	let mouseStartPointX;
+	let mouseStartPointY;
+	let selectedAreas = [];
+
+	const mouseDownEvent = (event) => {
+		console.log('Down');
+		mouseStartPointY = event.clientY;
+		mouseStartPointX = event.clientX
+
+	};
+
+	const mouseUpEvent = (event) => {
+		console.log(mouseStartPointX, mouseStartPointY);
+		if (mouseStartPointX && mouseStartPointY) {
+			const selectedArea = {
+				x: mouseStartPointX,
+				y: mouseStartPointY,
+				width: Math.abs(mouseStartPointX - event.clientX),
+				height: Math.abs(mouseStartPointY - event.clientY)
+			};
+
+			selectedAreas.push(selectedArea);
+			console.log('Mouse Up selected area', selectedArea);
+		}
+
+		console.log('Mouse up');
+		mouseStartPointY = null;
+		mouseStartPointY = null;
+	};
+
+	const mouseLeave = () => {
+		mouseStartPointX = null;
+		mouseStartPointY = null;
+	};
+
 	const render = () => {
 		let content = null;
 
 		if (state.visible) {
 			content = <div className={ css.imageAreaSelector }>
 				<div className={css.imageAreaSelectorImageArea}>
+					<div onMouseDown={(event) => mouseDownEvent(event)} onMouseUp={(event) => mouseUpEvent(event)} className={css.imageAreaSelectorSelectLayer}> </div>
 					<img src={state.imagePath} alt='' />
 				</div>
 				<div className={css.imageAreaSelectorActionArea}>
